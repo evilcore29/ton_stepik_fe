81df849779a7dc4ba8798a949a0d23ff0487ce42
evilcore29 <evilcore29@gmail.com>
2024-08-20 23:39:09 +0300

    TON Stepik Course: Lesson 5.4.1: Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import { nodePolyfills } from "vite-plugin-node-polyfills";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), nodePolyfills()],
-  base: "/ton_stepik_fe/",
+  base: "ton_stepik_fe",
 });
