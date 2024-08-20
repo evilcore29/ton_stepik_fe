bcb39bd83f988c771e7a36ddbdf2a82a96ae1efa
evilcore29 <evilcore29@gmail.com>
2024-08-20 22:18:08 +0300

    TON Stepik Course: Lesson 5.4.1: Add CI

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx'
-import './index.css'
-import { TonConnectUIProvider } from '@tonconnect/ui-react'
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { TonConnectUIProvider } from "@tonconnect/ui-react";
 
-const manifestUrl = 'https://raw.githubusercontent.com/evilcore29/TONStepik_FE/lesson-5.2/public/manifest.json'
+const manifestUrl = "https://evilcore29.github.io/TONStepik_FE/manifest.json";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
     <App />
-  </TonConnectUIProvider>,
-)
+  </TonConnectUIProvider>
+);
